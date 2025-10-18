65b5930f293fd88b1848d3dddfc5cb43da0f2ee3
charliestamp2002 <charliestamp2002@icloud.com>
2025-10-18 17:23:30 +0100

    Fix train_test_split order in simple_ml_script.py

--- Simple_Docker_Practice/simple_ml_script.py
+++ Simple_Docker_Practice/simple_ml_script.py
@@ -7,8 +7,9 @@ import pandas as pd
 
 iris = load_iris()
 
-X_train, y_train, X_test, y_test = train_test_split(
-iris.data, iris.target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=42
+)
 
 model = LogisticRegression(max_iter=200, C=1.0)
 model.fit(X_train, y_train)
