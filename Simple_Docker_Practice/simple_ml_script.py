7938f3e14d286e1533fa62c503dcc105172be4cc
charliestamp2002 <charliestamp2002@icloud.com>
2025-10-18 17:13:27 +0100

    Fix train_test_split order in simple_ml_script.py

---
+++ Simple_Docker_Practice/simple_ml_script.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib
+import pandas as pd
+
+iris = load_iris()
+
+X_train, y_train, X_test, y_test = train_test_split(
+iris.data, iris.target, test_size=0.2, random_state=42)
+
+model = LogisticRegression(max_iter=200, C=1.0)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Model accuracy: {acc:.4f}")
+
+#Save Predictions to CSV
+predictions_df = pd.DataFrame({"true": y_test, "predicted": y_pred})
+predictions_df.to_csv("predictions.csv", index=False)
+print("Predictions saved to 'predictions.csv'")
+
+# Save the model
+joblib.dump(model, "simple_logreg_model.joblib")
+print("Model saved to 'simple_logreg_model.joblib'")
